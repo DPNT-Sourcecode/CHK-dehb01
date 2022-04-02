849fd66fb08097acb07b8f1c260a30f6555d61f2
sbx_user1051 <sbx_user1051@169.254.18.5>
2022-04-02 10:32:00 +0000

    Sat Apr 02 10:32:00 UTC 2022

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -1,6 +1,9 @@
+from lib.solutions.HLO.hello_solution import hello
 from solutions.HLO import hello_solution
 
 
 class TestHello():
     def test_hello(self):
-        assert hello_solution.hello('Bob') == 'Hello, World!'
+        names = ['Jeff', 'Bob', 'Sam', 'Jerry']
+        for name in names:
+            assert hello_solution.hello(name) == f"Hello, {name}!"
