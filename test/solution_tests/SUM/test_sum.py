cf6c06342466aba1ee3e302746de10f51bdf8592
sbx_user1051 <sbx_user1051@169.254.18.5>
2022-04-02 10:22:24 +0000

    Sat Apr 02 10:22:24 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,4 +3,8 @@ from solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+        test_list = [(1,2), (3,4), (5,6), (7,8)]
+        solutions = [3, 7, 11, 15]
+        for test, solution in zip(test_list, solutions):
+            assert sum_solution.compute(test[0], test[1]) == solution
+
