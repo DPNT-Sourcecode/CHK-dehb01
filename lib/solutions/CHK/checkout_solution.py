c109b730cf47ca22522e748b0e95993e30c04496
sbx_user1051 <sbx_user1051@169.254.18.5>
2022-04-02 13:06:25 +0000

    Sat Apr 02 13:06:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,4 +1,5 @@
-from .helper_functions import calculate_A_discount
+from .helper_functions import calculate_discount
+from .dicts import PRICE_LIST, OFFER_DICT, BUY_MULTI_GET_FREE_OFFERS
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -6,16 +7,40 @@ def checkout(skus : str) -> int:
     if type(skus) != str: # input must be a string
         return -1
 
-    valid_skus = ['A', 'B', 'C', 'D', 'E', 'F']
+    valid_skus = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    counts = {}
+    # create a dict of zero counts for each sku
+    for sku in valid_skus:
+        counts[sku] = 0
     sku_list = list(skus)
-    price_list = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15,
-        "E": 40,
-        "F": 10,
-    }
+    price_list = PRICE_LIST.copy()
+    offer_dict = OFFER_DICT.copy()
+    buy_multi_get_free_offers = BUY_MULTI_GET_FREE_OFFERS.copy()
+
+    # count the totals of each sku
+    for sku in sku_list:
+        if sku not in valid_skus:
+            return -1
+        counts[sku] += 1
+    
+    # remove items from the basket that represent multi buy get one free
+    for sku, offer in buy_multi_get_free_offers.items():
+        sku_getting_for_free = offer[1]
+        free_count = int(counts[sku] / offer[0])
+        if counts[sku_getting_for_free] > free_count: # dont want to go into a negative count
+            counts[sku_getting_for_free] -= free_count
+        else: 
+            counts[sku_getting_for_free] = 0
+    
+    # add up the checkout total
+    checkout_total = 0
+    for sku, count in counts.items():
+        checkout_total += price_list[sku] * count
+    
+    # add up all the discounts
+    total_discount = 0
+    for sku, offer_list in offer_dict.items():
+        total_discount += cal
 
     A_count = 0
     B_count = 0
@@ -61,3 +86,4 @@ def checkout(skus : str) -> int:
     checkout_total = checkout_total - (F_discounts_to_apply * F_discount)
 
     return checkout_total
+
--- lib/solutions/CHK/helper_functions.py
+++ lib/solutions/CHK/helper_functions.py
@@ -1,58 +1,23 @@
-PRICE_LIST = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15,
-        "E": 40,
-        "F": 10,
-        'G': 20,
-        'H': 10,
-        'I': 35,
-        'J': 60,
-        'K': 80,
-        'L': 90,
-        'M': 15,
-        'N': 40,
-        'O': 10,
-        'P': 50,
-        'Q': 30,
-        'R': 50,
-        'S': 30,
-        'T': 20,
-        'U': 40,
-        'V': 50,
-        'W': 20,
-        'X': 90,
-        'Y': 10,
-        'Z': 50
-    }
+def calculate_discount(counts: dict, sku: str, offer_list: list, price_list: dict) -> int:
+    """returns the amount to discount from total because of the multi offer on an item"""
+    price = price_list[sku]
+    count = counts[sku]
 
-OFFER_DICT = {
-    "A": [(3,130), (5,200)],
-    "B": [(2, 45)],
-    "F": [(3, 20)],
-    "H": [(5,45), (10,80)],
-    "K": [(2,150)],
-    "P": [(5,200)],
-    "Q": [(3, 80)],
-    "U": [(4, 120)],
-    "V": [(2,90), (3,130)],
-}
+    if len(offer_list) == 2: # there is a large and small multi offer
+        large_offer = offer_list[1]
+        small_offer = offer_list[0]
+        large_count = int(count / large_offer[0])
+        large_discount = large_count * ((price * large_offer[0]) - large_offer[1])
+        remainder = count % large_offer[0]
+        small_count = int(remainder / small_offer[0])
+        small_discount = small_count * ((price * small_offer[0]) - small_offer[1])
+        return large_discount + small_discount
+    
+    if len(offer_list) == 1:
+        offer = offer_list[0]
+        discounts = int(count/ offer[0])
+        total = discounts * ((price * offer[0]) - offer[1])
+        return total
 
-
-BUY_MULTI_GET_FREE_OFFERS = {
-    "E": (2, 'B'),
-    "N": (3, 'M'),
-    "R": (3, 'Q')
-}
-
-def calculate_A_discount(A_count: int) -> int:
-    """returns the amount to discount from total because of the multi offer on item A"""
-    buy_5_discounts = int(A_count / 5)
-    buy_5_discount = buy_5_discounts * 50
-    remainder_of_A = A_count % 5
-    buy_3_discounts = int(remainder_of_A / 3)
-    buy_3_discount = buy_3_discounts * 20
-    return buy_5_discount + buy_3_discount
 
 
