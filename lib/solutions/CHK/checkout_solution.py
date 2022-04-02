a0685b69705ff96c67d0414ab81221320c34aa76
sbx_user1051 <sbx_user1051@169.254.18.5>
2022-04-02 10:54:09 +0000

    Sat Apr 02 10:54:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,38 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus : str) -> int:
+    if type(skus) != str: # input must be a string
+        return -1
 
-    raise NotImplementedError()
+    valid_skus = ['A', 'B', 'C', 'D']
+    sku_list = list(skus)
+    price_list = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15
+    }
+    A_count = 0
+    A_discount = 20 # offer is 3 for 130, a saving of 20 pounds
+    B_count = 0
+    B_discount = 15 # offer is 2 for 45, a saving of 15 pounds
+    checkout_total = 0
+    for sku in sku_list:
+        if sku not in valid_skus:
+            return -1
+        if sku == 'A':
+            A_count += 1
+        if sku == 'B':
+            B_count += 1
+        checkout_total += price_list[sku]
+    
+    A_discounts_to_apply = int(A_count / 3) # rounds down
+    B_discounts_to_apply = int(B_count / 2)
+
+    # Apply A discounts
+    checkout_total = checkout_total - (A_discounts_to_apply * A_discount)
+
+    # Apply B discounts
+    checkout_total = checkout_total - (B_discounts_to_apply * B_discount)
+
+    return checkout_total
