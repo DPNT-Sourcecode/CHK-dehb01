403f26d9bc5b02c1f1ffdac7639465fa793e3bb6
sbx_user1051 <sbx_user1051@169.254.18.5>
2022-04-02 11:19:25 +0000

    Sat Apr 02 11:19:25 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,7 +3,7 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        skus = ['A', 'B', 'C', 'D', 'AAA', 'BB', 1, 'ABC12', 'ACAAZ', 'AAABBAAA', 'BBBB', 'BBEE']
-        solutions = [50, 30, 20, 15, 130, 45, -1, -1, -1, 295, 90, 95]
+        skus = ['A', 'B', 'C', 'D', 'AAA', 'BB', 1, 'ABC12', 'ACAAZ', 'AAABBAAA', 'BBBB', 'BBEE', "EE", "EEEEBB", "BEBEEE"]
+        solutions = [50, 30, 20, 15, 130, 45, -1, -1, -1, 295, 90, 95, 80, 160, 160]
         for sku, solution in zip(skus, solutions):
             assert checkout_solution.checkout(sku) == solution
