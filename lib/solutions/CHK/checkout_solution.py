d2c7eae1b6dba72c8702f7eebb8f011ba631739e
sbx_user1051 <sbx_user1051@169.254.18.5>
2022-04-02 11:36:14 +0000

    Sat Apr 02 11:36:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ def checkout(skus : str) -> int:
     if type(skus) != str: # input must be a string
         return -1
 
-    valid_skus = ['A', 'B', 'C', 'D', 'E']
+    valid_skus = ['A', 'B', 'C', 'D', 'E', 'F']
     sku_list = list(skus)
     price_list = {
         "A": 50,
@@ -14,11 +14,16 @@ def checkout(skus : str) -> int:
         "C": 20,
         "D": 15,
         "E": 40,
+        "F": 10,
     }
+
     A_count = 0
     B_count = 0
     E_count = 0
+    F_count = 0
+
     B_discount = 15 # offer is 2 for 45, a saving of 15 pounds
+    F_discount = 10 # offer is effectively 3 for 20, a saving of 10 pounds
     checkout_total = 0
     for sku in sku_list:
         if sku not in valid_skus:
@@ -29,10 +34,13 @@ def checkout(skus : str) -> int:
             B_count += 1
         if sku == 'E': 
             E_count += 1
+        if sku == 'F':
+            F_count += 1
         checkout_total += price_list[sku]
     
     A_total_discount = calculate_A_discount(A_count)
     E_discounts_to_apply = int(E_count / 2)
+    F_discounts_to_apply = int(F_count / 3)
 
     B_count_for_discounts = B_count - E_discounts_to_apply # effectively remove the Bs from basket before applying th discount for B
     B_discounts_to_apply = int(B_count_for_discounts / 2)
@@ -48,4 +56,8 @@ def checkout(skus : str) -> int:
         checkout_total = checkout_total - (E_discounts_to_apply * 30) 
     if E_discounts_to_apply >= B_count:
         checkout_total = checkout_total - (B_count * 30) 
+
+    # Apply F discounts
+    checkout_total = checkout_total - (F_discounts_to_apply * F_discount)
+
     return checkout_total
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,7 +3,7 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        skus = ['A', 'B', 'C', 'D', 'AAA', 'BB', 1, 'ABC12', 'ACAAZ', 'AAABBAAA', 'BBBB', 'BBEE', "EE", "EEEEBB", "BEBEEE"]
-        solutions = [50, 30, 20, 15, 130, 45, -1, -1, -1, 295, 90, 110, 80, 160, 160]
+        skus = ['A', 'B', 'C', 'D', 'AAA', 'BB', 1, 'ABC12', 'ACAAZ', 'AAABBAAA', 'BBBB', 'BBEE', "EE", "EEEEBB", "BEBEEE", "FF", "FFF", "FFFFFF"]
+        solutions = [50, 30, 20, 15, 130, 45, -1, -1, -1, 295, 90, 110, 80, 160, 160, 20, 20, 40]
         for sku, solution in zip(skus, solutions):
             assert checkout_solution.checkout(sku) == solution
